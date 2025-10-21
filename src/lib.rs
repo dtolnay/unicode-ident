4d75a0ed3c633069904eaeb848a5f1c541d96add
David Tolnay <dtolnay@gmail.com>
2025-10-20 23:13:21 -0700

    Extract const promoted zero to a named static

--- src/lib.rs
+++ src/lib.rs
@@ -254,12 +254,14 @@ mod tables;
 
 use crate::tables::{ASCII_CONTINUE, ASCII_START, CHUNK, LEAF, TRIE_CONTINUE, TRIE_START};
 
+static ZERO: u8 = 0;
+
 /// Whether the character has the Unicode property XID\_Start.
 pub fn is_xid_start(ch: char) -> bool {
     if ch.is_ascii() {
         return ASCII_START & (1 << ch as u128) != 0;
     }
-    let chunk = *TRIE_START.0.get(ch as usize / 8 / CHUNK).unwrap_or(&0);
+    let chunk = *TRIE_START.0.get(ch as usize / 8 / CHUNK).unwrap_or(&ZERO);
     let offset = chunk as usize * CHUNK / 2 + ch as usize / 8 % CHUNK;
     unsafe { LEAF.0.get_unchecked(offset) }.wrapping_shr(ch as u32 % 8) & 1 != 0
 }
@@ -269,7 +271,10 @@ pub fn is_xid_continue(ch: char) -> bool {
     if ch.is_ascii() {
         return ASCII_CONTINUE & (1 << ch as u128) != 0;
     }
-    let chunk = *TRIE_CONTINUE.0.get(ch as usize / 8 / CHUNK).unwrap_or(&0);
+    let chunk = *TRIE_CONTINUE
+        .0
+        .get(ch as usize / 8 / CHUNK)
+        .unwrap_or(&ZERO);
     let offset = chunk as usize * CHUNK / 2 + ch as usize / 8 % CHUNK;
     unsafe { LEAF.0.get_unchecked(offset) }.wrapping_shr(ch as u32 % 8) & 1 != 0
 }
